ef0f39fa9b4d13e8c6fdd1d49c349c97f18d0233
FernandoHerbas <feer15.herbas@gmail.com>
2020-08-01 02:29:20 -0300

    Añado un archivo leds.php donde recibo por POST valores necesarios para encender un led en arduino. Los envio mediante ajax. Comento lineas de link en panel necesarias para el grafico del historial de temp(aun no usare esta opcion)

---
+++ back/leds.php
@@ -0,0 +1,9 @@
+<?php
+    $valor = $_POST['dato'];
+    var_dump($valor);
+    switch($valor){
+        case 'a':
+            echo "hola";
+        break;
+    }
+?>
--- front/js/scripts.js
+++ front/js/scripts.js
@@ -1,8 +1,23 @@
+const URL = "../../back/leds.php";
+
 function cambiarColorLed(idLed){
     var ledColor = document.getElementById(idLed).style.backgroundColor;
-    if(ledColor == 'gray'){
+    if(ledColor == '' || ledColor == 'gray'){
         document.getElementById(idLed).style.backgroundColor='blue';
-    }else{
+        enviarDatosAlServer('a');
+    }else if(ledColor == 'blue'){
         document.getElementById(idLed).style.backgroundColor='gray';
+        enviarDatosAlServer('b');
     }
+}
+
+function enviarDatosAlServer($valor){
+    $.ajax({
+        url: '../back/leds.php',
+        data:{ dato: $valor},
+        type: 'POST',
+        success : function(respuesta) {
+           console.log(respuesta);
+        }
+    });
 }
